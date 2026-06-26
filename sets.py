c485ea9337a02e08b014e3054eba822777465a19
vivekkumaronh-arch <vivekkumaron.h@gmail.com>
2026-06-26 11:18:27 +0530

    function and sets

---
+++ sets.py
@@ -0,0 +1,50 @@
+a={8,6,4,7,9,2,4,12,54,34,23,76,87,98,46,7,5,35,75,87,23,45,6,7,0,145,765,34,57,98}    # repitition doesn't show in output
+print(a)
+print(sorted(a))     # for sorting
+
+print(type(a))
+
+# creating an empty set
+x={}   # dictionary not a set
+print(type(x))
+
+b=set()
+print(type(b))
+
+# creating set from list
+L=[4,5,6,3,7,7,8,8,8]
+s=set(L)
+print(s)
+
+# Important functions of set
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Mathematical operations for set
+# Union
+
+
+
+
+
+
+
+
+
+
+
+
+
+
